974a791c122faf760433ff233ba9d420f3bdf5a0
amine-boujemaaoui <amine.boujemaaoui.1@gmail.com>
2023-01-28 17:46:05 +0100

    Lecture et ecriture des donne dans la structure de données
    A changer car il y a trop d'aretes pour faire un malloc simple pour toutes les aretes

--- src/files/structures.c
+++ src/files/structures.c
@@ -3,32 +3,54 @@
 // Fonction pour creer et allouer une station
 Station *new_station()
 {
-    Station *station = (Station *)malloc(sizeof(Station));
-    assert(station != NULL);
+    Station *station = (Station*)malloc(sizeof(Station));
+    if (station == NULL)
+    {
+        printf("ERREUR: impossible de creer la station\n");
+        exit(1);
+    }
     return station;
 }
 
 // Fonction pour creer et allouer une arete
 Arete *new_arete()
 {
-    Arete *arete = (Arete *)malloc(sizeof(Arete));
-    assert(arete != NULL);
+    Arete *arete = (Arete*)malloc(sizeof(Arete));
+    if (arete == NULL)
+    {
+        printf("ERREUR: impossible de creer l'arete\n");
+        exit(1);
+    }
     return arete;
 }
 
 // Fonction pour creer et allouer un graphe
 Graphe *new_graphe(int nbStations, int nbAretes)
 {
-    Graphe *graphe = (Graphe *)malloc(sizeof(Graphe));
-    assert(graphe != NULL);
-    graphe->stations = (Station **)malloc(nbStations * sizeof(Station *));
-    assert(graphe->stations != NULL);
+    Graphe *graphe = (Graphe*)malloc(sizeof(Graphe));
+    if (graphe == NULL)
+    {
+        printf("ERREUR: impossible de creer le graphe\n");
+        exit(1);
+    }
+    graphe->stations = (Station**)malloc(nbStations * sizeof(Station*));
+    if (graphe->stations == NULL)
+    {
+        printf("ERREUR: impossible de creer les stations du graphe\n");
+        exit(1);
+    }
     for (int i = 0; i < nbStations; i++)
         graphe->stations[i] = new_station();
-    graphe->aretes = (Arete **)malloc(nbStations * sizeof(Arete *));
-    assert(graphe->aretes != NULL);
+    graphe->aretes = (Arete**)malloc(nbStations * sizeof(Arete*));
+    if (graphe->aretes == NULL)
+    {
+        printf("ERREUR: impossible de creer les aretes du graphe\n");
+        exit(1);
+    }
     for (int i = 0; i < nbAretes; i++)
+    {
         graphe->aretes[i] = new_arete();
+    }
     graphe->nbStations = nbStations;
     graphe->nbAretes = nbAretes;
     return graphe;
@@ -39,7 +61,7 @@ Graphe *chargerGraphe(char *nomFichierStations, char *nomFichierAretes)
 {
     FILE *fichierStations = fopen(nomFichierStations, "r");
     FILE *fichierAretes = fopen(nomFichierAretes, "r");
-    char tmpChar[50] = "";
+    char tmpChar[50];
     int tmpInt, tmpInt2;
     if (fichierStations == NULL)
     {
@@ -51,41 +73,34 @@ Graphe *chargerGraphe(char *nomFichierStations, char *nomFichierAretes)
         printf("Impossible d'ouvrir le fichier %s\n", nomFichierAretes);
         exit(1);
     }
-
     // On compte le nombre de station dans le fichier nomFichierAretes
     int nbStations = 0;
-    fscanf(fichierStations, "%[^,],%[^\n]", tmpChar, tmpChar);
+    fscanf(fichierStations, "%[^,],%[^\n]", tmpChar, tmpChar); // on passe l'entete du fichier
     while (fscanf(fichierStations, "%[^,],%d\n", tmpChar, &tmpInt) != EOF)
         nbStations++;
-    nbStations--; // On ne compte pas la premier ligne du fichier qui est l'entete
-    //rewind(fichierStations);
-
+    rewind(fichierStations);
     // On compte le nombre d'arrete dans le fichier nomFichierAretes
     int nbAretes = 0;
-    fscanf(fichierAretes, "%[^,],%[^,],%[^\n]", tmpChar, tmpChar, tmpChar);
+    fscanf(fichierAretes, "%[^,],%[^,],%[^\n]", tmpChar, tmpChar, tmpChar); // on passe l'entete du fichier
     while (fscanf(fichierAretes, "%d,%d,%[^\n]", &tmpInt, &tmpInt2, tmpChar) != EOF)
         nbAretes++;
-    nbAretes--; // On ne compte pas la premier ligne du fichier qui est l'entete
-    //rewind(fichierAretes);
-
+    rewind(fichierAretes);
     Graphe *graphe = new_graphe(nbStations, nbAretes);
-    int count = 0;
-
     // On charge les stations dans le graphe
-    fscanf(fichierStations, "%[^,],%d", tmpChar, &tmpInt);
-    while (fscanf(fichierStations, "%[^,],%d", graphe->stations[count]->nom, &graphe->stations[count]->id) != EOF)
-    {
-        // printf("%s,%d\n", graphe->stations[count]->nom, graphe->stations[count]->id);
+    int count = 0;
+    fscanf(fichierStations, "%[^,],%[^\n]", tmpChar, tmpChar); // on passe l'entete du fichier
+    while (fscanf(fichierStations, "%[^,],%d\n", graphe->stations[count]->nom, &graphe->stations[count]->id) != EOF)
         count++;
-    }
-
+    printf("c\n");
     // On charge les aretes dans le graphe
     count = 0;
-    while (fscanf(fichierAretes, "%d,%d,%[^\n]\n", &graphe->aretes[count]->origine, &graphe->aretes[count]->destination, graphe->aretes[count]->ligne) != EOF)
+    fscanf(fichierAretes, "%[^,],%[^,],%[^\n]", tmpChar, tmpChar, tmpChar); // on passe l'entete du fichier
+    while (fscanf(fichierAretes, "%d,%d,%[^\n]", &graphe->aretes[count]->origine, &graphe->aretes[count]->destination, graphe->aretes[count]->ligne) != EOF)
+    {
+        graphe->aretes[count]->poids = MINUTE_CONNEXION;
         count++;
-
+    }
     fclose(fichierStations);
     fclose(fichierAretes);
-
     return graphe;
 }
--- src/test/test_lecture_fichier.c
+++ src/test/test_lecture_fichier.c
@@ -4,12 +4,15 @@ int main()
 {
     printf("\ndebut chargement ...\n");
     Graphe *graphe = chargerGraphe("data/stations.csv", "data/aretes.csv");
-    /*
+
+    printf("Nom,Id\n");
     for (int count = 0; count < graphe->nbStations; count++)
-    {
-        printf("%d|id:%d|nom:%s|\n", count, graphe->stations[count]->id, graphe->stations[count]->nom);
-    }
+        printf("%s,%d\n", graphe->stations[count]->nom, graphe->stations[count]->id);
+
+    printf("Origine,Destination,Ligne\n");
+    for (int count = 0; count < graphe->nbAretes; count++)
+        printf("%d,%d,%s\n", graphe->aretes[count]->origine, graphe->aretes[count]->destination, graphe->aretes[count]->ligne);
+
     printf("\nfin chargement.\n");
-    */
     return EXIT_SUCCESS;
 }
