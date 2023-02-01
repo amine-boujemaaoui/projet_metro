b8443f4c7f3122780ea67625fefe3913c52dd5e6
amine-boujemaaoui <amine.boujemaaoui.1@gmail.com>
2023-02-01 21:35:03 +0100

    Deplacement de la fonction chargerGraphe vers functions.c

--- include/functions.h
+++ include/functions.h
@@ -2,4 +2,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+// fonction d'allocation mémoire et affichage de message d'erreur en cas d'echec
 void *myMalloc(uint64_t size, char *message);
+// Fonction pour lire les données des stations à partir d'un fichier
+Graphe *chargerGraphe(char *nomFichierStations, char *nomFichierAretes);
--- src/files/functions.c
+++ src/files/functions.c
@@ -1,5 +1,7 @@
 #include "../../include/functions.h"
+#include "../../include/structures.h"
 
+// fonction d'allocation mémoire et affichage de message d'erreur en cas d'echec
 void *myMalloc(uint64_t size, char *message)
 {
     void *pointeur = malloc(size);
@@ -10,4 +12,53 @@ void *myMalloc(uint64_t size, char *message)
     }
     else
         return pointeur;
+}
+
+// Fonction pour lire les données des stations à partir d'un fichier
+Graphe *chargerGraphe(char *nomFichierStations, char *nomFichierAretes)
+{
+    FILE *fichierStations = fopen(nomFichierStations, "r");
+    FILE *fichierAretes = fopen(nomFichierAretes, "r");
+    char tmpChar[50];
+    uint32_t tmpInt, tmpInt2;
+    if (fichierStations == NULL)
+    {
+        printf("Impossible d'ouvrir le fichier %s\n", nomFichierStations);
+        exit(1);
+    }
+    if (nomFichierAretes == NULL)
+    {
+        printf("Impossible d'ouvrir le fichier %s\n", nomFichierAretes);
+        exit(1);
+    }
+    // On compte le nombre de station dans le fichier nomFichierAretes
+    uint32_t nbStations = 0;
+    fscanf(fichierStations, "%[^,],%[^\n]", tmpChar, tmpChar); // on passe l'entete du fichier
+    while (fscanf(fichierStations, "%[^,],%d\n", tmpChar, &tmpInt) != EOF)
+        nbStations++;
+    rewind(fichierStations);
+    // On compte le nombre d'arrete dans le fichier nomFichierAretes
+    uint32_t nbAretes = 0;
+    fscanf(fichierAretes, "%[^,],%[^,],%[^\n]", tmpChar, tmpChar, tmpChar); // on passe l'entete du fichier
+    while (fscanf(fichierAretes, "%d,%d,%[^\n]", &tmpInt, &tmpInt2, tmpChar) != EOF)
+        nbAretes++;
+    rewind(fichierAretes);
+    Graphe *graphe = new_graphe(nbStations, nbAretes);
+    // On charge les stations dans le graphe
+    uint32_t count = 0;
+    fscanf(fichierStations, "%[^,],%[^\n]", tmpChar, tmpChar); // on passe l'entete du fichier
+    while (fscanf(fichierStations, "%[^,],%d\n", graphe->stations[count]->nom, &graphe->stations[count]->id) != EOF)
+        count++;
+    printf("c\n");
+    // On charge les aretes dans le graphe
+    count = 0;
+    fscanf(fichierAretes, "%[^,],%[^,],%[^\n]", tmpChar, tmpChar, tmpChar); // on passe l'entete du fichier
+    while (fscanf(fichierAretes, "%d,%d,%[^\n]", &graphe->aretes[count]->origine, &graphe->aretes[count]->destination, graphe->aretes[count]->ligne) != EOF)
+    {
+        graphe->aretes[count]->poids = MINUTE_CONNEXION;
+        count++;
+    }
+    fclose(fichierStations);
+    fclose(fichierAretes);
+    return graphe;
 }
